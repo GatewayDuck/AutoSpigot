d3b268e27e4f4e84b4bf4c5494a9d3d952b369ca
GatewayDuck <88541634+GatewayDuck@users.noreply.github.com>
2022-06-29 23:50:52 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 # Working for ubuntu 20.04
-sudo apt update && sudo apt upgrade
-sleep 1
-sudo apt install wget apt-transport-https gnupg nano screen
-sudo ufw allow OpenSSH
-sudo ufw allow 25565
-sudo ufw enable
-sudo apt install openjdk-17-jre-headless
-sleep 1
-sudo apt install git
-sleep 1
+# sudo apt update && sudo apt upgrade
+# sleep 1
+# sudo apt install wget apt-transport-https gnupg nano screen
+# sudo ufw allow OpenSSH
+# sudo ufw allow 25565
+# sudo ufw enable
+# sudo apt install openjdk-17-jre-headless
+# sleep 1
+# sudo apt install git
+# sleep 1
 
 
 echo "Enter the folder name that the minecraft server will be placed, THIS CAN NOT BE ANY SPECIAL CHARS OR A BLANK SPACE:"
