a9b30ec3b2eb9e37d4cf92d608377384602dc4bb
GatewayDuck <88541634+GatewayDuck@users.noreply.github.com>
2022-06-30 00:49:06 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,5 +32,4 @@ cd ..
 mkdir $dir/server
 mv $dir/buildtools/spigot-$mcverison.jar $dir/server/spigot.jar
 
-# echo -e "start:\njava -Xms3G -Xmx3G -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar ./server/spigot.jar nogui"
-
+wget https://raw.githubusercontent.com/GatewayDuck/AutoSpigot/main/control.sh -O $dir/controll
