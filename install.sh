d0ff889fcd419e7cdb5bcafbfbd96e857f789568
GatewayDuck <88541634+GatewayDuck@users.noreply.github.com>
2022-07-17 02:42:08 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,19 @@
-# Working for ubuntu 20.04
-sudo apt update && sudo apt upgrade
-sleep 1
-sudo apt install wget apt-transport-https gnupg nano screen
-sudo ufw allow OpenSSH
-sudo ufw allow 25565
-sudo ufw enable
-sudo apt install openjdk-17-jre-headless
-sleep 1
-sudo apt install git
-sleep 1
+echo "Do you want to run the first time configs [Y/n]:"
+read runConfig
+if [ $runConfig = "Y" ] then 
+    sudo apt update && sudo apt upgrade
+    sleep 1
+    sudo apt install wget apt-transport-https gnupg nano screen
+    sudo ufw allow OpenSSH
+    sudo ufw allow 25565
+    sudo ufw enable
+    sudo apt install openjdk-17-jre-headless
+    sleep 1
+    sudo apt install git
+    sleep 1
+fi
 
-
-echo "Enter the folder name that the minecraft server will be placed, THIS CAN NOT BE ANY SPECIAL CHARS OR A BLANK SPACE:"
+echo "Enter the folder name that the minecraft server will be placed:"
 read folder
 mdir="./minecraftServers/"
 dir="./minecraftServers/$folder"
@@ -20,7 +22,7 @@ mkdir $dir
 
 mkdir $dir/buildtools
 wget -O $dir/buildtools/BuildTools.jar  https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-echo "Please select a minecraft verison (1.19 to 1.18), PLEASE DO NOT ENTER ANY OTHER TEXT BUT THE VERISON (E.x : 1.19):"
+echo "Please select a minecraft verison:"
 read mcverison
 cd $dir/buildtools
 java -jar BuildTools.jar --rev $mcverison
@@ -39,4 +41,9 @@ echo "By using this program, you agree you minecraft's elua.\nTo continue press 
 read yea
 
 wget https://raw.githubusercontent.com/GatewayDuck/AutoSpigot/main/eula.txt -O $dir/server/eula.txt
+
+
+
+
+# Working for ubuntu 20.04
 
